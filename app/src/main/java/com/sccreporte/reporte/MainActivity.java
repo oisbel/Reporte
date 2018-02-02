946ac8735a3c955a916900a56c4cce9f6d9b3316
oisbel <oisbelsimpv@gmail.com>
2018-02-01 23:26:52 -0600

    Add last reports activity

--- app/src/main/java/com/sccreporte/reporte/MainActivity.java
+++ app/src/main/java/com/sccreporte/reporte/MainActivity.java
@@ -1,13 +1,32 @@
 package com.sccreporte.reporte;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView lastReportsTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lastReportsTV = (TextView)findViewById(R.id.lastReportsTextView);
+
+        lastReportsTV.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Context context = MainActivity.this;
+                Class destinationActivity = LastReportsActivity.class;
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+                startActivity(startChildActivityIntent);
+            }
+        });
     }
 }
