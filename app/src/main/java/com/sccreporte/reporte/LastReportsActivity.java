271ae75eb2d1ac6c458391ccbfcd607a0c4f3780
oisbel <oisbelsimpv@gmail.com>
2018-02-02 10:05:02 -0600

    Add header for last report activity

--- app/src/main/java/com/sccreporte/reporte/LastReportsActivity.java
+++ app/src/main/java/com/sccreporte/reporte/LastReportsActivity.java
@@ -2,12 +2,23 @@ package com.sccreporte.reporte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class LastReportsActivity extends AppCompatActivity {
 
+    ImageButton backBT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_reports);
+        backBT = (ImageButton)findViewById(R.id.backButton);
+        backBT.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
