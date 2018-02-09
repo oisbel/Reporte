9c4388db24d259584f5b153d153732f71028deca
oisbel <oisbelsimpv@gmail.com>
2018-02-09 08:58:03 -0600

    Add back button functionality for report activity

--- app/src/main/java/com/sccreporte/reporte/ReportActivity.java
+++ app/src/main/java/com/sccreporte/reporte/ReportActivity.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class ReportActivity extends AppCompatActivity {
 
+    ImageButton backBT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        // Back button click
+        backBT = (ImageButton)findViewById(R.id.backButton);
+        backBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
