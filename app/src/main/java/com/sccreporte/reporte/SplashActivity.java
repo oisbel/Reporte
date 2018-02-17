262f5981c755154a49778a2ad420fd42c84452e0
oisbel <oisbelsimpv@gmail.com>
2018-02-17 00:32:35 -0600

    Go to register screen if not user

--- app/src/main/java/com/sccreporte/reporte/SplashActivity.java
+++ app/src/main/java/com/sccreporte/reporte/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.sccreporte.reporte;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,25 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Check if there is a user registered
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String user_email = sharedPreferences.getString("user_email","");
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(user_email.equals("")){
+                    // No hay nadie registrado
+                    Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 2000);
     }
 }
