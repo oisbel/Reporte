008de94940162b4d7b77aefe71cb19241f749e79
oisbel <oisbelsimpv@gmail.com>
2018-02-17 00:07:23 -0600

    Add register screens

---
+++ app/src/main/java/com/sccreporte/reporte/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.sccreporte.reporte;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+}
