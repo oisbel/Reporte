eae0cdc9db6e88b9ddf47d7a4237dcae7aa1ea8f
oisbel <oisbelsimpv@gmail.com>
2018-02-18 09:17:58 -0600

    Create click event for register card view

--- app/src/main/java/com/sccreporte/reporte/CreatePasswordActivity.java
+++ app/src/main/java/com/sccreporte/reporte/CreatePasswordActivity.java
@@ -3,22 +3,32 @@ package com.sccreporte.reporte;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CreatePasswordActivity extends AppCompatActivity {
 
+    // String extra pasado de la activity register
+    String mUserDataJSONString="";
+    // Objeto JSON creado a partir de mUserDataJSONString
+    JSONObject userDataJASON;
+    EditText emailET;
+    EditText passwordET;
+    CardView registerCV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_password);
 
-        // String extra pasado de la activity register
-        String mUserDataJSONString="";
-        // Objeto JSON creado a partir de mUserDataJSONString
-        JSONObject userDataJASON;
+        emailET = (EditText) findViewById(R.id.emailEditText);
+        passwordET = (EditText) findViewById(R.id.passwordEditText);
+        registerCV = (CardView) findViewById(R.id.registerCardView);
 
         // Obtener el string pasado de la activity anterior
         Intent intentThatStartedThisActivity = getIntent();
@@ -33,5 +43,12 @@ public class CreatePasswordActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
+        registerCV.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
