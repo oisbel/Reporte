69b340737e967d288a769cc34f2548a7763e5205
oisbel <oisbelsimpv@gmail.com>
2018-02-20 10:01:15 -0600

    Add links from Splash activity to login and register

--- app/src/main/java/com/sccreporte/reporte/SplashActivity.java
+++ app/src/main/java/com/sccreporte/reporte/SplashActivity.java
@@ -1,18 +1,31 @@
 package com.sccreporte.reporte;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    TextView loginTB;
+    Button registerBT;
+    ProgressBar loadingIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        loginTB = (TextView)findViewById(R.id.loginTextView);
+        registerBT = (Button)findViewById(R.id.registerButton);
+        loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
 
         // Check if there is a user registered, load the user email
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -22,6 +35,7 @@ public class SplashActivity extends AppCompatActivity {
         //editor.clear();
         //editor.apply();
 
+        // Load user data to see if is already register
         final String user_email = sharedPreferences.getString("email","");
 
         Handler handler = new Handler();
@@ -30,9 +44,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 if(user_email.equals("")){
                     // No hay nadie registrado
-                    Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
-                    startActivity(intent);
-                    finish();
+                    showLoginRegister();
                 } else {
                     // if there is data from existing user: log in
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
@@ -41,5 +53,35 @@ public class SplashActivity extends AppCompatActivity {
                 }
             }
         }, 1500);
+
+        loginTB.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = SplashActivity.this;
+                Class destinationActivity = CreatePasswordActivity.class;
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+                startActivity(startChildActivityIntent);
+                finish();
+            }
+        });
+
+        registerBT.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Muestra los links para entrar y registrase(estan invisibles)
+     * y hide el progress loading bar
+     */
+    private void showLoginRegister(){
+        loginTB.setVisibility(View.VISIBLE);
+        registerBT.setVisibility(View.VISIBLE);
+        loadingIndicator.setVisibility(View.INVISIBLE);
     }
 }
