decca093ec94769c1544b8c2231b3bf80b5c5347
oisbel <oisbelsimpv@gmail.com>
2018-03-23 10:39:09 -0500

    Add numero field to class User

--- app/src/main/java/com/sccreporte/reporte/data/User.java
+++ app/src/main/java/com/sccreporte/reporte/data/User.java
@@ -13,10 +13,11 @@ public class User {
     public String responsabilidad;
     public String lugar;
     public String pastor;
+    public String numero;
     public String password;
 
     public User(int id, String nombre, String email, String grado, String ministerio,
-                String responsabilidad, String lugar, String pastor, String password){
+                String responsabilidad, String lugar, String pastor, String numero, String password){
         this.id = id;
         this.nombre = nombre;
         this.email = email;
@@ -25,6 +26,7 @@ public class User {
         this.responsabilidad = responsabilidad;
         this.lugar = lugar;
         this.pastor = pastor;
+        this.numero = numero;
         this.password = password;
     }
 }
