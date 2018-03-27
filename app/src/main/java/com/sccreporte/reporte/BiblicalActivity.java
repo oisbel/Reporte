edc73ac62f3bd3c1e60f5b9c26fca0ada2b0a6a5
oisbel <oisbelsimpv@gmail.com>
2018-03-27 10:48:08 -0500

    Add user information to biblical layout

--- app/src/main/java/com/sccreporte/reporte/BiblicalActivity.java
+++ app/src/main/java/com/sccreporte/reporte/BiblicalActivity.java
@@ -4,15 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.sccreporte.reporte.data.User;
+import com.sccreporte.reporte.utilities.DataUtils;
 
 public class BiblicalActivity extends AppCompatActivity {
+    private TextView nameUserTV;
+    private TextView lugarUserTV;
 
     private ImageButton backBT;
+
+    private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biblical);
+
+        nameUserTV = findViewById(R.id.nameUserTextView);
+        lugarUserTV = findViewById(R.id.lugarUserTextView);
 
         // Back button click
         backBT = (ImageButton)findViewById(R.id.backButton);
@@ -22,5 +33,12 @@ public class BiblicalActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // agregar el nombre y el lugar del usuario
+        mUser = DataUtils.loadUserData(this);
+        if(mUser.id == -1)
+            finish();
+        nameUserTV.setText(mUser.nombre);
+        lugarUserTV.setText(mUser.lugar);
     }
 }
