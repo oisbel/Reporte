56026da047e24af5b4d086a468c37314ebc1316b
oisbel <oisbelsimpv@gmail.com>
2018-04-09 21:22:15 -0500

    Add network methods to creaate biblical

--- app/src/main/java/com/sccreporte/reporte/utilities/NetworkUtils.java
+++ app/src/main/java/com/sccreporte/reporte/utilities/NetworkUtils.java
@@ -296,7 +296,64 @@ public class NetworkUtils {
             urlConnection.disconnect();
         }
     }
-    //TODO : Eliminar todas los get FromHTTpURl, dado que son lo mismo
+
+    /**
+     * Builds the URL used to query sccreporte.
+     * @return The URL to use to query the sccreporte create biblical.
+     */
+    public static URL buildCreateBiblicalUrl() {
+        Uri builtUri = Uri.parse(BASE_CREATE_BIBLICAL_URL).buildUpon()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+    /**
+     * Returns the entire result from the HTTP response after create biblical.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @param jsonParam The json object to sent it with the request.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String geCreateBiblicalFromHttpUrl(URL url, JSONObject jsonParam,
+                                                   String username, String password) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+        String basicAuth = buildBasicAuthorizationString(username, password);
+        urlConnection.setRequestProperty("Authorization", basicAuth);
+        urlConnection.setRequestProperty("Accept","application/json");
+        urlConnection.setDoOutput(true);
+        urlConnection.setDoInput(true);
+
+        DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
+        out.writeBytes(jsonParam.toString());
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    //TODO : Eliminar todas los get FromHTTpURl, y build urls dado que son lo mismo
 
     private static String buildBasicAuthorizationString(String username, String password) {
 
