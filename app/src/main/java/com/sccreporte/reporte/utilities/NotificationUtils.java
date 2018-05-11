6df6b3782f156d462b42898a9f2de8863e2129eb
oisbel <oisbelsimpv@gmail.com>
2018-05-11 09:57:25 -0500

    Add method to decode a bitmap needed for the notification

--- app/src/main/java/com/sccreporte/reporte/utilities/NotificationUtils.java
+++ app/src/main/java/com/sccreporte/reporte/utilities/NotificationUtils.java
@@ -3,8 +3,12 @@ package com.sccreporte.reporte.utilities;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import com.sccreporte.reporte.MainActivity;
+import com.sccreporte.reporte.R;
 
 /**
  * Created by simpson on 5/11/2018.
@@ -32,5 +36,18 @@ public class NotificationUtils {
                 CREATE_REPORT_REMINDER_PENDING_INTENT_ID,
                 startActivityIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    /**
+     * takes in a Context as a parameter and
+     * returns a Bitmap. This method is necessary to decode a bitmap needed for the notification.
+     * @param context
+     * @return
+     */
+    private static Bitmap largeIcon(Context context) {
+        // Get a Resources object from the context.
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.ic_action_addreport);
+        return largeIcon;
     }
 }
