34170bc3b8e81ef8e8c966661d9eeb52b9ccf67a
oisbel <oisbelsimpv@gmail.com>
2018-05-18 09:58:00 -0500

    Create a class ReminderTask for issuing a create-report reminder notification

---
+++ app/src/main/java/com/sccreporte/reporte/sync/ReminderTasks.java
@@ -0,0 +1,23 @@
+package com.sccreporte.reporte.sync;
+
+import android.content.Context;
+
+import com.sccreporte.reporte.utilities.NotificationUtils;
+
+/**
+ * Created by simpson on 5/18/2018.
+ */
+
+public class ReminderTasks {
+
+    public static final String ACTION_CREATE_REPORT_REMINDER = "create-report-reminder";
+
+    public static void executeTask(Context context, String action) {
+        if(ACTION_CREATE_REPORT_REMINDER.equals(action)){
+            issueCreateReportReminder(context);
+        }
+    }
+    private static void issueCreateReportReminder(Context context) {
+        NotificationUtils.remindUserCreateReport(context);
+    }
+}
