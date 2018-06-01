b86c716f3141405091a2d2a8a45fc402b4056486
oisbel <oisbelsimpv@gmail.com>
2018-05-31 23:48:51 -0500

    Add helper class for adding swipe functionality to recycler view

---
+++ app/src/main/java/com/sccreporte/reporte/RecyclerItemTouchHelper.java
@@ -0,0 +1,71 @@
+package com.sccreporte.reporte;
+
+/**
+ * Created by simpson on 5/31/2018.
+ */
+import android.graphics.Canvas;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback{
+    private RecyclerItemTouchHelperListener listener;
+
+    public RecyclerItemTouchHelper(int dragDirs, int swipeDirs, RecyclerItemTouchHelperListener listener) {
+        super(dragDirs, swipeDirs);
+        this.listener = listener;
+    }
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return true;
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (viewHolder != null) {
+            final View foregroundView = ((BiblicalsAdapter.BiblicalViewHolder) viewHolder).viewForeground;
+
+            // This method is used for ItemTouchHelper to detect whenever there is UI change on the view.
+            // We use this function to keep the background view in a static position and move the foreground view.
+            getDefaultUIUtil().onSelected(foregroundView);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        // the x-position of the foreground view is changed while user is swiping the view.
+        final View foregroundView = ((BiblicalsAdapter.BiblicalViewHolder) viewHolder).viewForeground;
+        getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        final View foregroundView = ((BiblicalsAdapter.BiblicalViewHolder) viewHolder).viewForeground;
+        getDefaultUIUtil().clearView(foregroundView);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        final View foregroundView = ((BiblicalsAdapter.BiblicalViewHolder) viewHolder).viewForeground;
+
+        getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        listener.onSwiped(viewHolder, direction, viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    public interface RecyclerItemTouchHelperListener {
+        void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
+    }
+}
