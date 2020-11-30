648be4da2feb63813abeb47e29231053efd7d7f1
Oisbel Simpson <oisbelsimpv@gmail.com>
2020-11-29 22:40:51 -0600

    Add new ViewPage tutorial

--- app/src/main/java/com/sccreporte/reporte/TutorialActivity.java
+++ app/src/main/java/com/sccreporte/reporte/TutorialActivity.java
@@ -3,16 +3,67 @@ package com.sccreporte.reporte;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator;
+
 
 public class TutorialActivity extends AppCompatActivity {
+
+    private TextView explicadosTV;
+    private ImageButton closeBT;
+
+    ViewPager viewPager;
+    SpringDotsIndicator circleIndicator3;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
+        explicadosTV = findViewById(R.id.explicadosTextView);
+        closeBT = findViewById(R.id.closeButton);
+
+        viewPager = findViewById(R.id.view_pager);
+        circleIndicator3 = findViewById(R.id.indicator);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        Integer[] images = {R.drawable.tutorial_create_report, R.drawable.tutorial_edit_report,
+                R.drawable.tutorial_edit_biblical, R.drawable.tutorial_delete_biblical};
+        String[] titles = {getResources().getString(R.string.topic_create_report),
+                getResources().getString(R.string.topic_edit_report),
+                getResources().getString(R.string.topic_add_biblical),
+                getResources().getString(R.string.topic_delete_biblical)};
+        String[] details = {getResources().getString(R.string.details_create_report_help),
+                getResources().getString(R.string.details_edit_report_help),
+                getResources().getString(R.string.details_add_biblical_help),
+                getResources().getString(R.string.details_delete_biblical_help)};
+        viewPagerAdapter.FillData(images,titles,details);
+
+        viewPager.setAdapter(viewPagerAdapter);
+        circleIndicator3.setViewPager(viewPager);
+
+        //Click para abrir explicados activity
+        explicadosTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class destinationActivity = ExplicadosActivity.class;
+                Intent startChildActivityIntent = new Intent(getApplicationContext(), destinationActivity);
+                startActivity(startChildActivityIntent);
+            }
+        });
+
+        closeBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sccreporte/reporte/ViewPagerAdapter.java
@@ -0,0 +1,65 @@
+package com.sccreporte.reporte;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images ;
+    private String[] titles ;
+    private String[] details ;
+
+    public ViewPagerAdapter(Context context){
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.item_page, null);
+        ImageView imageView = view.findViewById(R.id.ivImage);
+        imageView.setImageResource(images[position]);
+        TextView title = view.findViewById(R.id.tvTitle);
+        title.setText(titles[position]);
+        TextView detail = view.findViewById(R.id.tvdetails);
+        detail.setText(details[position]);
+
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+
+    public void FillData(Integer[] images, String[] titles, String[] details){
+        this.images = images;
+        this.titles = titles;
+        this.details = details;
+    }
+}
